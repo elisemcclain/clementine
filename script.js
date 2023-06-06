fe8454fd8eedfdd6f642f040023e316baf929486
Elise McClain <celisemcclain@gmail.com>
2023-06-05 23:06:02 -0500

    button to website

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
 document.querySelector("body").appendChild(h2);
+
+const button = document.createElement('button')
+button.innerText = 'Click if you love Cows'
+button.id = 'mainButton'
+
+button.addEventListener('click', () => {
+  alert('You love cows!')
+})
+
+document.body.appendChild(button)
